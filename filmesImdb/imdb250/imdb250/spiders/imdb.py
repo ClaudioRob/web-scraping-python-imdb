51f327b62b46e9d157ed99cbf91b2fdfff8e83be
ClaudioRob <klaudio.cnet@gmail.com>
2024-11-20 21:14:28 -0300

    update file

--- filmesImdb/imdb250/imdb250/spiders/imdb.py
+++ filmesImdb/imdb250/imdb250/spiders/imdb.py
@@ -3,13 +3,15 @@ import json
 
 class ImdbSpider(scrapy.Spider):
     name = "imdb"
-    alloewd_domais = ["imdb.com"]
+    allowed_domains = ['imdb.com']
     start_urls = ["https://www.imdb.com/chart/top/"]
 
     def parse(self, response):
 
         raw_data = response.css("script[id='__NEXT_DATA__']::text").get()
+
         json_data = json.loads(raw_data)
+
         need_data = json_data['props']['pageProps']['pageData']['chartTitles']['edges']
 
         for movie in need_data:
@@ -18,8 +20,8 @@ class ImdbSpider(scrapy.Spider):
                 'movie_rank': movie['currentRank'],
                 'release_yaer': movie['node']['releaseYear']['year'],
                 'movie_length': movie['node']['runtime']['seconds'],
-                'rating': movie['node']['ratingSumary']['aggregateRating'],
-                'vout_count': movie['node']['RatingsSumary']['voteCount'],
+                'rating': movie['node']['ratingsSummary']['aggregateRating'],
+                'vote_count': movie['node']['ratingsSummary']['voteCount'],
                 'description': movie['node']['plot']['plotText']['plainText']
             }
                                                     
